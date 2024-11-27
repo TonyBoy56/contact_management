916ab2cc70da49d2bca4e77b166032fa71c77567
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-27 12:03:36 -0800

    write an insert method that will create a new node at the root of the tree if a root does not current exist.

--- contact_management.py
+++ contact_management.py
@@ -1,6 +1,18 @@
+# class for the binary tree node
 class Node:
     def __init__(self, key, data):
         self.key = key
         self.data = data
         self.left = None
         self.right = None
+        
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, key, data):
+        new_node = Node(key, data)
+        if not self.root:
+            self.root = new_node
+        else: 
+            self._insert_recursive(self.root, new_node)
